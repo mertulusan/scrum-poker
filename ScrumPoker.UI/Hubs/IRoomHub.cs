44909c7571a54325d0c7c0c1236655eab3815250
sinem canoğlu <sinem.canoglu@foriba.com>
2021-07-04 01:53:11 +0300

    hub özelleştirildi. memory işlemleri tamamen kaldırıldı

---
+++ ScrumPoker.UI/Hubs/IRoomHub.cs
@@ -0,0 +1,18 @@
+﻿using ScrumPoker.Model.Enums;
+using ScrumPoker.Model.Model;
+using System.Threading.Tasks;
+
+namespace ScrumPoker.UI.Hubs
+{
+    public interface IRoomHub
+    {
+        Task JoinRoomAsync(string roomName);
+        Task ReceiveMessage(Room room);
+
+
+        //Task SendMessageRoomMateAsync(string roomName, User user, CardPoints cardPoint);
+        //Task LeaveRoomAsync(string groupName);
+        //Task SendMessage(string user, string message);
+        //Task AddTimeRoom(string roomName);
+    }
+}
