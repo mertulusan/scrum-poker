8c0086b1805a3d690000ab8198952b73b718865e
Okay Mardin <okay.mardin@sovos.com>
2021-07-05 16:27:24 +0300

    oylama vb. tamamlandı.

--- ScrumPoker.Model/Model/User.cs
+++ ScrumPoker.Model/Model/User.cs
@@ -9,7 +9,7 @@ namespace ScrumPoker.Model.Model
         public Guid Id { get; set; }
         public string Name { get; set; }
         public RoleType Role { get; set; }
-        public int Point { get; set; }
+        public CardPoints? Point { get; set; }
         public bool IsActive { get; set; }
     }
 }
